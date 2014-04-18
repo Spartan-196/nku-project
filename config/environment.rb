d91a91b44fb8a3ce9ce6e26ebbb98c7f37305015
James Atkinson <atkinsonj3@nku.edu>
2014-04-18 19:36:52 +0000

    Add Action mailer to enviroment

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 NkuProject::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => 'sendgird_username',
+  :password       => 'sendgrid_pass',
+  :domain         => 'example.com',
+  :enable_starttls_auto => true
+}
