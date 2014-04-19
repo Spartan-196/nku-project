3fec28d8ab60c55b0e1b67144ebdf4987b737bda
James Atkinson <atkinsonj3@nku.edu>
2014-04-19 06:08:52 +0000

    corrected route path typeo for index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 NkuProject::Application.routes.draw do
-  root to: 'user#index' #Rails Root location to start looking pages
+  root to: 'users#index' #Rails Root location to start looking pages
     post "users/upload", to: "users#upload", as: :users_upload
 resources :sessions, :invoices
 
