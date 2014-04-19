431113eb587078385292e47ef2bb5e9ff23c2120
James Atkinson <atkinsonj3@nku.edu>
2014-04-19 05:54:41 +0000

    add default, sessions, invoices, and upload routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 NkuProject::Application.routes.draw do
-  root to: 'invoices#index' #Rails Root location to start looking pages
+  root to: 'user#index' #Rails Root location to start looking pages
+    post "users/upload", to: "users#upload", as: :users_upload
+resources :sessions, :invoices
+
 end
