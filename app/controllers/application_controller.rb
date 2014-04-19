66ff0ece9c20e05cca8ce6c87146868405336371
James Atkinson <atkinsonj3@nku.edu>
2014-04-19 05:47:37 +0000

    Port logged in user find and rquired login from nku-rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+    def get_current_user
+    # Find whos logged in
+    if( session[:user_id] == nil )
+      return nil
+    end
+    return Student.find( session[:user_id] )
+  end
+
+  helper_method :get_current_user
+
+  private
+  def require_login
+    if get_current_user == nil
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to sessions_path # halts request cycle
+    end
+   end
+  end
 end
