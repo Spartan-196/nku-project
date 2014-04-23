498f4b5d1eab16470e5f227252803b1eabcc141f
James Atkinson <atkinsonj3@nku.edu>
2014-04-23 04:30:23 +0000

    Created customer model
    This has many invoices, and validates the following
    first name
    lastname
    email(presence,uniqueness, format, and confirmation)
    address 1
    city
    state
    zip
    
    and admin handeling

---
+++ app/models/customer.rb
@@ -0,0 +1,22 @@
+class Customer < ActiveRecord::Base
+  has_many :invoices
+  #validates_associated :invoices
+  validates  :first_name
+  validates  :last_name presence: true, length: { minimum: 2 }
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: {with: /\A[^@]+@[^@]+\z/ message:"must enter valid email address"}
+  validates :email, confirmation: true { message: "must confirm email"}
+  validates  :address_1, presence: true
+  validates  :city, presence: true
+  validates  :state, presence: true
+  validates  :zip, presence: true, length: { minimum: 5 }
+  validates_presence_of :password, :on => :create
+
+	def self.admin?
+  	  if self.admin == true
+    	  true
+    	end
+ 	 end
+
+end
