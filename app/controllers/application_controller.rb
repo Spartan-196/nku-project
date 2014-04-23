c445ba36dfdcd74e427830dcbf3eeb2ebac87dc4
James Atkinson <atkinsonj3@nku.edu>
2014-04-23 00:38:07 +0000

    Added required login before using anything

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+     before_action :require_login
+
     def get_current_user
     # Find whos logged in
     if( session[:user_id] == nil )
