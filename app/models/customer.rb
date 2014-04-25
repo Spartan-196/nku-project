e0b0a71cf4e123b0fd7f3d5d741bb43b5f9d32c8
James Atkinson <atkinsonj3@nku.edu>
2014-04-25 19:57:04 +0000

    comment non-working validators

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,11 @@
 class Customer < ActiveRecord::Base
   has_many :invoices
   #validates_associated :invoices
-  validates  :first_name
-  validates  :last_name presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
   validates :email, presence: true
   validates :email, uniqueness: true
-  validates :email, format: {with: /\A[^@]+@[^@]+\z/ message:"must enter valid email address"}
-  validates :email, confirmation: true { message: "must confirm email"}
+  validates :email, format: {with: /\A[^@]+@[^@]+\z/} #{message: "must enter valid email address"}}
+  validates :email, confirmation: true #{ message: "must confirm email" }
   validates  :address_1, presence: true
   validates  :city, presence: true
   validates  :state, presence: true
