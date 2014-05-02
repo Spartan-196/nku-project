dd1d0b79e83e42166f42507d6ef9bfb1924be1a4
James Atkinson <atkinsonj3@nku.edu>
2014-05-02 19:02:47 +0000

    Corrected login issue when trying to make new user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
      before_action :require_login
-
+      skip_before_action :require_login, only: [:new, :create]
     def get_current_user
     # Find whos logged in
     if( session[:user_id] == nil )
