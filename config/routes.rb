b9d9aab076ac362dfdca4eed8e06eb942a6656cd
James Atkinson <atkinsonj3@nku.edu>
2014-05-02 02:27:30 +0000

    Removed routes and links to invoices
    This part of the project has been scrapped by both me and the
    inteneded final user of this project, as its going to be handed over
    so someone else to actually use in an enviorment they have setup for
    a one man small business

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 NkuProject::Application.routes.draw do
-  root to: 'users#index' #Rails Root location to start looking pages
+  root to: 'customers#index' #Rails Root location to start looking pages
     post "users/upload", to: "users#upload", as: :users_upload
-resources :sessions, :invoices, :users, :customers
+    post "customers/upload", to: "customers#upload", as: :customers_upload
+resources :sessions, :users, :customers #,:invoices
 
 end
